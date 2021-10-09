a2f870a3c87aab429c6675b895ae82b2d7584d5c
upsadin <upsadin@gmail.com>
2021-10-09 21:22:58 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        int year = 2021;
+        System.out.println(idea);
+        idea = idea + " But I am a newbie.";
+        System.out.println(idea);
+        idea = idea + " " + year;
+        System.out.println(idea);
+    }
+}
