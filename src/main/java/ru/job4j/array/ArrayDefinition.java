9d216f1c78b3e965aeeb81aee40472dbabcc1d16
upsadin <upsadin@gmail.com>
2021-10-31 16:58:58 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -13,6 +13,9 @@ public class ArrayDefinition {
         names[1] = "Ivan Ivanov";
         names[2] = "Pavel Pavlov";
         names[3] = "San Sanich";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
         System.out.println(names[3]);
     }
 }
