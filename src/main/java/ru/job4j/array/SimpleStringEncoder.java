5cf6e6569a0452c949ec66dcd8a4ea5414c14212
upsadin <upsadin@gmail.com>
2023-09-01 20:46:26 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049]

---
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class SimpleStringEncoder {
+    public static String encode(String input) {
+        String result = "";
+        char symbol = input.charAt(0);
+        int counter = 1;
+        for (int i = 1; i < input.length(); i++) {
+            if (symbol == input.charAt(i)) {
+                counter++;
+            } else {
+                result += Character.toString(symbol) + (counter == 1 ? "" : counter);
+                symbol = input.charAt(i);
+                counter = 1;
+            }
+        }
+        result += Character.toString(symbol) + (counter == 1 ? "" : counter);
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/array/SimpleStringEncoderTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Assert;
+
+public class SimpleStringEncoderTest {
+    @Test
+    public void whenStringaaabbcThenResulta3b2c() {
+        String input = "aaabbc";
+        String expected = "a3b2c";
+        String result = SimpleStringEncoder.encode(input);
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenStringabbcccThenResulta2b3c() {
+        String input = "abbccc";
+        String expected = "ab2c3";
+        String result = SimpleStringEncoder.encode(input);
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenStringaaabccThenResulta3bc2() {
+        String input = "aaabcc";
+        String expected = "a3bc2";
+        String result = SimpleStringEncoder.encode(input);
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenStringabcThenResultabc() {
+        String input = "abc";
+        String expected = "abc";
+        String result = SimpleStringEncoder.encode(input);
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenStringaaabbbcccThenResulta3b3c3() {
+        String input = "aaabbbccc";
+        String expected = "a3b3c3";
+        String result = SimpleStringEncoder.encode(input);
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenStringaaaaaaaaaaaabbbcddddThenResulta12b3cd4() {
+        String input = "aaaaaaaaaaaabbbcdddd";
+        String expected = "a12b3cd4";
+        String result = SimpleStringEncoder.encode(input);
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenStringaaabbbbaaabbcccdddddThenResulta3b4a3b2c3d5() {
+        String input = "aaabbbbaaabbcccddddd";
+        String expected = "a3b4a3b2c3d5";
+        String result = SimpleStringEncoder.encode(input);
+        Assert.assertEquals(result, expected);
+    }
+}
